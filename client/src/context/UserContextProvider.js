9bf09665180f3c9762f7bfd831399b8005190327
anshumancodes <anshumanprof01@gmail.com>
2024-04-29 23:28:52 +0530

    modified usercontext

--- client/src/context/UserContextProvider.js
+++ client/src/context/UserContextProvider.js
@@ -1,13 +1,17 @@
-import  { useState, createContext, useContext } from 'react';
+import  {  createContext, useContext } from 'react';
 
 export const UserContext = createContext({
   user:null,
-  setUser:()=>{}
+ 
+  
 });
 
 export const UserContextProvider = UserContext.Provider;
 
 
 export default function useAuth() {
+ 
+  const context =useContext(UserContext);
+  if(!context) throw Error("please use a context !!!!!!!!!!!!!!!!!!!!!")
   return useContext(UserContext);
 }
