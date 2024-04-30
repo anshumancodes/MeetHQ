2df9e579db78829d6d93287df46c6a8cd1bde383
anshumancodes <anshumanprof01@gmail.com>
2024-04-30 23:52:59 +0530

    modified user controller

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const ApiResponse =require("../services/ApiResponse")
+const ApiError =require("../services/ApiError");
 
 const User = require("../models/user.models");
 const {setUser,getUser}=require("../services/auth")
@@ -18,19 +20,47 @@ async function HandleSignUp(req,res){
 }
 
 async function HandleLogin(req,res){
-    const {email,password}=req.body;
-    const isUser=await User.findOne({email,password});
+    // const {email,password}=req.body;
+    // const isUser=await User.findOne({email,password});
 
-    // if  there is no such user or the password found in db:
-    if(!isUser){
-        return res.status(401).json({message:"Invalid Email or Password"})
+    // // if  there is no such user or the password found in db:
+    // if(!isUser){
+    //     return res.status(401).json({message:"Invalid Email or Password"})
+    // }
+    // // if found set the cookie
+    // const jwttoken=setUser(isUser);
+    // res.cookie("cookieID",jwttoken);
+    
+
+    try {
+        const {email,password}=req.body;
+        const isUser=await User.findOne({email,password});
+        const jwttoken=setUser(isUser);
+        res.cookie("cookieID",jwttoken);
+        const message = "User Logged In Successfully";
+        const userData={
+            user:jwttoken
+
+           
+        }
+
+        const apiResponse = new ApiResponse(200, message, userData);
+        res.status(apiResponse.statuscode).json(apiResponse);
+        
+    } catch (error) {
+        
+        const apiError=new ApiError(500,"user not found!",error.message);
+        res.status(apiError.statuscode).json(apiError);
+
+        
     }
-    // if found set the cookie
-    const jwttoken=setUser(isUser);
-    res.cookie("cookieID",jwttoken);
+
+
 
 
 
 }
+
+
 
 module.exports={HandleSignUp,HandleLogin}
